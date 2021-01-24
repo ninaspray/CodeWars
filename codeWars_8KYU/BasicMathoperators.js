f4b029b1c60dc6a09754c5035bb46bb7ab80a691
Nina Spray <ninaspray@gmail.com>
2021-01-24 18:05:57 +0000

    Basic Math Operators if statement

---
+++ codeWars_8KYU/BasicMathoperators.js
@@ -0,0 +1,12 @@
+/*Your task is to create a function that does four basic mathematical operations.*/
+
+function basicOp(operation, value1, value2) {
+    if (operation == '+') { return value1 + value2 }
+    else if (operation == '-') { return value1 - value2 }
+    else if (operation == '*') { return value1 * value2 }
+    else (operation == '/')
+    { return value1 / value2 }
+};
+
+  //Tested on console - all worked. 
+  //PROGRESS
